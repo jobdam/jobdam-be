a7454120c8cd1e78ad10798a9e6f2177be23f1ed
goForItkang <kangyohan.dev.0421@gmail.com>
2025-04-21 17:24:25 +0900

    mybatis config

--- src/main/java/com/jobdam/jobdam_be/user/model/User.java
+++ src/main/java/com/jobdam/jobdam_be/user/model/User.java
@@ -1,4 +1,16 @@
 package com.jobdam.jobdam_be.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private long userId;
+    private String name;
+    private String password;
+    private String role;
+    private String userName;
 }
