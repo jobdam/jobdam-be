b36abadaec6f040e5a81497eb9cb75e01cefefb8
leeSH <dlfyd05680@gmail.com>
2025-04-23 15:53:08 +0900

    [FEAT] Auth 기본 설정

--- src/main/java/com/jobdam/jobdam_be/config/SecurityConfig.java
+++ src/main/java/com/jobdam/jobdam_be/config/SecurityConfig.java
@@ -5,7 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -13,14 +19,42 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http    .csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers(
-                                        "/","/login","/signup","/js/**","/WEB-INF/views/**"
-                                        ,"/send","/checksms","/test"
-                                ).permitAll()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                .sessionManagement(sessionManagement -> sessionManagement.
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(
+                                "/", "/login", "/sign-up", "/js/**", "/WEB-INF/views/**"
+                                , "/send", "/check-sns", "/test"
+                        ).permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
+        configuration.setAllowCredentials(true);
+
+        // 개발 중	0 또는 생략 (변경 반영 빠르게)
+        // 배포 시 (안정된 정책)	600 ~ 3600 초
+        // 보안이 매우 중요한 경우	60 초 이하로 낮게 유지
+        configuration.setMaxAge(600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
