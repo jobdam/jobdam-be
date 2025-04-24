4feb11452f19bcd8b2a7ed07f7133828a731d283
leeSH <dlfyd05680@gmail.com>
2025-04-24 14:49:40 +0900

    [FIX] import 누락 해소

--- src/main/java/com/jobdam/jobdam_be/config/SecurityConfig.java
+++ src/main/java/com/jobdam/jobdam_be/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.jobdam.jobdam_be.config;
 
+import com.jobdam.jobdam_be.auth.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
