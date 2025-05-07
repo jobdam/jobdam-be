f28247918b923a3f24ca2f768dba7609edee6a71
lys118 <dldudtn118a@gmail.com>
2025-05-07 17:28:17 +0900

    [REFACTOR] auth암호들 구조번경

--- src/main/java/com/jobdam/jobdam_be/global/config/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jobdam.jobdam_be.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
-public class EnvConfig {
-}
