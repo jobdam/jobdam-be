b4877bcf3f4f741a1c630c438671ecab16a0b375
lys118 <dldudtn118a@gmail.com>
2025-05-08 16:47:48 +0900

    [FEAT]서비스프로퍼티변경

--- src/test/java/com/jobdam/jobdam_be/JobdamBeApplicationTests.java
+++ src/test/java/com/jobdam/jobdam_be/JobdamBeApplicationTests.java
@@ -1,8 +1,10 @@
 package com.jobdam.jobdam_be;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class JobdamBeApplicationTests {
 
