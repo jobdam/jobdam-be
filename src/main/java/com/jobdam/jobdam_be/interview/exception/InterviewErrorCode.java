e7510e7ab1d8bc37685e73bd0524b254b8cf9c6e
lys118 <dldudtn118a@gmail.com>
2025-05-19 13:25:56 +0900

    [FIX] 인터뷰id조회 추가및수정

--- src/main/java/com/jobdam/jobdam_be/interview/exception/InterviewErrorCode.java
+++ src/main/java/com/jobdam/jobdam_be/interview/exception/InterviewErrorCode.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 @Getter
 @AllArgsConstructor
 public enum InterviewErrorCode implements ErrorCode {
+    NOT_FOUND_EXCEPTION(404,"인터뷰를 찾을 수 없습니다"),
     DB_INSERT_ERROR(500, "데이터베이스 입력오류가 발생했습니다.");
     private final int code;
     private final String message;
